77968b0ddca9ac8d40f9cf32cb32b3b848b28e63
Andrew Holgate <andrewholgate@yahoo.com>
2016-03-19 09:50:17 +0100

    Update setup commands to include: - Adding SSH key to agent - Adding git configs - Correct symlinking of directories

--- setup.sh
+++ setup.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 set -e
 
-# Setup project symlinks.
-#ln -s /var/www/project/repo/web /var/www/current
-#ln -s /var/www/current /var/www/html
-#chown -R ubuntu:www-data /var/www/html
+## Setup project symlinks.
+#sudo rm -Rf /var/www/html
+#sudo ln -s /var/www/project/repo/web /var/www/current
+#sudo ln -s /var/www/current /var/www/html
+#sudo chown -R ubuntu:www-data /var/www/html /var/www/current
+
+## Add SSH keys to SSH agent
+#eval "$(ssh-agent -s)"
+#ssh-add ~/.ssh/id_rsa
+
+## Change permissions to config and SSH key as needed.
+#chown ubuntu:ubuntu ~/.ssh/config
+
+# Add git user configs
+#git config --global user.email "name@example.com"
+#git config --global user.name "Name Surname"
 
 exec "$@"
